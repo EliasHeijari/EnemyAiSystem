2176b029f76818ab89efce055a66e9da64530898
Elias Heijari <69460338+elkkuzq@users.noreply.github.com>
2024-02-08 12:39:30 +0200

    Enemy Attack Update and Ranges Changed

--- Assets/EnemySystem/Scripts/EnemyAttackHandler.cs
+++ Assets/EnemySystem/Scripts/EnemyAttackHandler.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAttackHandler : MonoBehaviour
 {
+    NavMeshAgent navMeshAgent;
+
+    private void Start() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
     public void Shoot(Transform target){
+        navMeshAgent.velocity = Vector3.zero;
         Debug.Log($"Shoot to {target.name}");
     }
 }
